705d35484bdf92d6df7ea487a1a9b2e73f16bd32
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2024-05-06 18:06:14 +0100

    feat: flesh out game slightly

--- RLib-Core/src/main/java/io/github/thatsmusic99/resurgencelib/game/Game.java
+++ RLib-Core/src/main/java/io/github/thatsmusic99/resurgencelib/game/Game.java
@@ -4,6 +4,7 @@ import io.github.thatsmusic99.resurgencelib.map.Map;
 import io.github.thatsmusic99.resurgencelib.player.IGamePlayer;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Game<P extends IGamePlayer<?>, M extends Map<P>> {
@@ -12,5 +13,12 @@ public abstract class Game<P extends IGamePlayer<?>, M extends Map<P>> {
     @NotNull protected List<P> players;
     protected int timer;
 
+    public Game(final @NotNull M map) {
+        this.players = new ArrayList<>();
+        this.map = map;
+    }
 
+    public @NotNull List<P> getPlayers() {
+        return this.players;
+    }
 }
